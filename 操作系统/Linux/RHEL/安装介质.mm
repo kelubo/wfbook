7d036c8aa34076002ad92b60656710c8733e5255
WangFei <wf.ab@126.com>
2020-09-18 15:05:32 +0800

    RHEL安装介质

---
+++ 操作系统/Linux/RHEL/安装介质.mm
@@ -0,0 +1,14 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1600412353594" ID="ID_305960734" MODIFIED="1600412362313" TEXT="&#x5b89;&#x88c5;&#x4ecb;&#x8d28;">
+<node CREATED="1600412383652" ID="ID_1971437598" MODIFIED="1600412402473" POSITION="right" TEXT="&#x542b;&#x6709;Anaconda&#x7684;&#x4e8c;&#x8fdb;&#x5236;DVD">
+<node CREATED="1600412424001" ID="ID_546777454" MODIFIED="1600412569552" TEXT="RHEL&#x5b89;&#x88c5;&#x7a0b;&#x5e8f;&#xff0c;&#x4ee5;&#x53ca;BaseOS&#x548c;AppStream&#x8f6f;&#x4ef6;&#x5305;&#x5b58;&#x50a8;&#x5e93;&#x3002;&#x5305;&#x542b;&#x6709;&#x5b8c;&#x6210;&#x5b89;&#x88c5;&#x6240;&#x9700;&#x7684;&#x8f6f;&#x4ef6;&#x5305;&#xff0c;&#x65e0;&#x9700;&#x5176;&#x4ed6;&#x6750;&#x6599;&#x3002;"/>
+</node>
+<node CREATED="1600412490284" ID="ID_1685876277" MODIFIED="1600412525925" POSITION="right" TEXT="&#x542b;&#x6709;Anaconda&#x7684;&#x542f;&#x52a8;ISO">
+<node CREATED="1600412528381" ID="ID_515223867" MODIFIED="1600412564411" TEXT="&#x9700;&#x8981;&#x914d;&#x7f6e;&#x7f51;&#x7edc;&#xff0c;&#x4ee5;&#x4fbf;&#x8bbf;&#x95ee;&#x901a;&#x8fc7;HTTP&#x3001;FTP&#x6216;NFS&#x63d0;&#x4f9b;&#x7684;&#x8f6f;&#x4ef6;&#x5305;&#x5b58;&#x50a8;&#x5e93;&#x3002;"/>
+</node>
+<node CREATED="1600412574366" ID="ID_843520410" MODIFIED="1600412628120" POSITION="right" TEXT="&#x542b;&#x6709;&#x9884;&#x6784;&#x5efa;&#x7cfb;&#x7edf;&#x78c1;&#x76d8;&#x7684;QCOW2&#x6620;&#x50cf;">
+<node CREATED="1600412629634" ID="ID_1077392981" MODIFIED="1600412651360" TEXT="&#x53ef;&#x4ee5;&#x5728;&#x4e91;&#x6216;&#x4f01;&#x4e1a;&#x865a;&#x62df;&#x73af;&#x5883;&#x4e2d;&#x90e8;&#x7f72;&#x4e3a;&#x865a;&#x62df;&#x673a;"/>
+</node>
+</node>
+</map>
