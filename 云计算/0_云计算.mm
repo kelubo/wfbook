e782d8c535b5b0fd1dc4fc4e71996d556a8bcde2
WangFei <wf.ab@126.com>
2023-05-09 10:57:16 +0800

    云计算

---
+++ 云计算/0_云计算.mm
@@ -0,0 +1,100 @@
+<map version="freeplane 1.11.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="云计算" FOLDED="false" ID="ID_1572428074" CREATED="1683600443655" MODIFIED="1683600459119"><hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="提供服务形式" POSITION="bottom_or_right" ID="ID_714121211" CREATED="1683600584870" MODIFIED="1683600599647">
+<node TEXT="公有云" ID="ID_1475861952" CREATED="1683600589264" MODIFIED="1683600604647">
+<node TEXT="企业将自己的内部IT资源服务于外部客户，外部客户具有使用权，无所有权。" ID="ID_83741347" CREATED="1683600662456" MODIFIED="1683600698875"/>
+<node TEXT="供养商" ID="ID_1525003600" CREATED="1683600700645" MODIFIED="1683600705760">
+<node TEXT="AWS" ID="ID_238689873" CREATED="1683600706525" MODIFIED="1683600709511"/>
+<node TEXT="Azure" ID="ID_724430742" CREATED="1683600709861" MODIFIED="1683600712510"/>
+<node TEXT="阿里云" ID="ID_154302011" CREATED="1683600712765" MODIFIED="1683600718160"/>
+</node>
+</node>
+<node TEXT="私有云" ID="ID_1648323219" CREATED="1683600604862" MODIFIED="1683600608871">
+<node TEXT="企业将自己的IT资源服务于自己，具有使用权和所有权。" ID="ID_1677046830" CREATED="1683600733877" MODIFIED="1683600759246"/>
+<node TEXT="技术实现" ID="ID_1337105919" CREATED="1683600760469" MODIFIED="1683600766309">
+<node TEXT="OpenStack" ID="ID_1086566360" CREATED="1683600766312" MODIFIED="1683600771166"/>
+<node TEXT="CloudStack" ID="ID_300398479" CREATED="1683600771502" MODIFIED="1683600776679"/>
+</node>
+</node>
+<node TEXT="混合云" ID="ID_1435099605" CREATED="1683600609557" MODIFIED="1683600613095">
+<node TEXT="融合了公有云和私有云，企业根据不同的业务需求而综合利用公有云和私有云提供的服务" ID="ID_1197765290" CREATED="1683600780749" MODIFIED="1683600824534"/>
+<node TEXT="提供商" ID="ID_563517437" CREATED="1683600825317" MODIFIED="1683600829655">
+<node TEXT="IBM" ID="ID_1737094593" CREATED="1683600830261" MODIFIED="1683600831982"/>
+<node TEXT="华为" ID="ID_1991832975" CREATED="1683600832429" MODIFIED="1683600837783"/>
+</node>
+</node>
+</node>
+<node TEXT="云平台" POSITION="top_or_left" ID="ID_1918944468" CREATED="1683600932301" MODIFIED="1683600937381">
+<node TEXT="IAAS" ID="ID_954713051" CREATED="1683600937384" MODIFIED="1683600943511"/>
+<node TEXT="PAAS" ID="ID_1726618097" CREATED="1683600943940" MODIFIED="1683600946687"/>
+<node TEXT="SAAS" ID="ID_502633649" CREATED="1683600948036" MODIFIED="1683600950455"/>
+<node TEXT="CAAS" ID="ID_859320926" CREATED="1683600951101" MODIFIED="1683600953454"/>
+</node>
+</node>
+</map>
